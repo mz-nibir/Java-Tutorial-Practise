0b8097e9ea5d4d98769cb3873839161bf32db683
Nibir <mz.nibir@gmail.com>
2020-10-23 16:45:08 +0600

    forthlecture

---
+++ FourthLecture.java
@@ -0,0 +1,25 @@
+
+public class FourthLecture {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b;
+		a=10;
+		b=20;
+		int[] numberArray= new int[100];
+		
+
+	}
+	static int calculateNumber(int number1,int number2) {
+		int result= number1+number2;
+		return result;
+	}
+	
+	static void prln(Object anyObject) {
+		System.out.println(anyObject);
+	}
+	static void pr(Object anyObject) {
+		System.out.print(anyObject);
+	}
+
+}
