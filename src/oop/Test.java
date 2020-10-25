d7bef9d02f415aedc11e7139b35d210673493734
Nibir <mz.nibir@gmail.com>
2020-10-26 00:12:42 +0600

    setter n getter

---
+++ src/oop/Test.java
@@ -0,0 +1,18 @@
+package oop;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Parson p1 =new Parson();
+		
+		p1.setName("Nibir");
+		
+		//String nam = p1.getName();
+		
+		System.out.println("Name : "+p1.getName());
+
+	}
+
+}
